c77fbae94af649896692d72fee65720aa04b3a27
gustavobragaia <52480533+gustavobragaia@users.noreply.github.com>
2019-07-13 03:46:00 -0300

    js

---
+++ projetos testes/js/estrutura-lexica.js
@@ -0,0 +1,13 @@
+/*var Variavel = "Animal";
+var variavel = "Animal";
+function func(){
+    return variavel !== Variavel;
+}
+console.log(func());*/
+
+var Variavel = "Animal";
+//var variavel = "Animal";
+function func(){
+    return variavel !== Variavel;
+}
+console.log(func());
---
+++ projetos testes/js/operadores-unarios.js
@@ -0,0 +1,8 @@
+/*var x = "5" + '5'
+
+function variavel()
+{
+    return --x
+}
+console.log(variavel());
+*/
